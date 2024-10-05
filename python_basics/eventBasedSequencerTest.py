0a78981b328ed7b2cad3eaa9aee4ab51ba4ab7a4
MorrisEnno vanWeel <morrisweel@gmail.com>
2024-10-05 11:09:35 +0200

    EventBasedSeqSecondDraft

--- python_basics/eventBasedSequencerTest.py
+++ python_basics/eventBasedSequencerTest.py
@@ -22,25 +22,25 @@
 import pygame
 import time
 pygame.init()
-Kick = pygame.mixer.Sound("C:/Users/Morris/Desktop/HKU/HKU_jaar2/CSD2/CSD2/Kick.wav")
-Snare = pygame.mixer.Sound("C:/Users/Morris/Desktop/HKU/HKU_jaar2/CSD2/CSD2/Snare.wav")
-HiHat = pygame.mixer.Sound("C:/Users/Morris/Desktop/HKU/HKU_jaar2/CSD2/CSD2/HiHat.wav")
+kick = pygame.mixer.Sound("C:/Users/Morris/Desktop/HKU/HKU_jaar2/CSD2/CSD2/Kick.wav")
+snare = pygame.mixer.Sound("C:/Users/Morris/Desktop/HKU/HKU_jaar2/CSD2/CSD2/Snare.wav")
+hiHat = pygame.mixer.Sound("C:/Users/Morris/Desktop/HKU/HKU_jaar2/CSD2/CSD2/HiHat.wav")
 hallo = pygame.mixer.Sound("C:/Users/Morris/Desktop/AttractorSamples/CA.textureHit.wav")
 
-hallo.play()
+#hallo.play()
 print("Hallo!")
 
 
 #User inputs
-bpm = 120
+bpm = 172
 numRepeats = 1
 
 #Define note durs and offsets for different samples used
-KickNoteDurs = [1, 1, 1, 1, 1, 1, 1, 1]
-KickNoteOffset = [0]
+KickNoteDurs = [2.5, 1.5, 2.5, 1.5]
+KickNoteOffset = 0
 
-ClapNoteDurs = [2, 2, 2, 2]
-ClapNoteOffset = 1
+SnareNoteDurs = [2, 2, 2, 2]
+SnareNoteOffset = 1
 
 HiHatNoteDurs = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
 HiHatNoteOffset = 0
@@ -60,20 +60,20 @@ def OffsetTo16ths(offset):
 KickNote16th = DursTo16ths(KickNoteDurs)
 KickNoteOffset16th = OffsetTo16ths(KickNoteOffset)
 
-ClapNote16th = DursTo16ths(ClapNoteDurs)
-ClapNoteOffset16th = OffsetTo16ths(ClapNoteOffset)
+SnareNote16th = DursTo16ths(SnareNoteDurs)
+SnareNoteOffset16th = OffsetTo16ths(SnareNoteOffset)
 
 HiHatNote16th = DursTo16ths(HiHatNoteDurs)
 HiHatNoteOffset16th = OffsetTo16ths(HiHatNoteOffset)
 
 
 print("KickNote16th:", KickNote16th)
-print("ClapNote16th:", ClapNote16th)
+print("ClapNote16th:", SnareNote16th)
 print("HiHatNote16th:", HiHatNote16th)
 
 
-# Function to convert timestamps to time durations (in seconds)
-def ToTimeDur(timestamps, BPM, offset):
+# Function to convert timestamps to time durations, back to timestamps
+def ToTimestamp(timestamps, BPM, offset):
     QuarterNoteDur = 60 / BPM
     SixteenthNoteDur = QuarterNoteDur  / 4
     
@@ -83,32 +83,61 @@ def ToTimeDur(timestamps, BPM, offset):
       stamps.append(timeValue)
       print("Time durations:", stamps)
     return stamps
+
+kickTimestamps = ToTimestamp(KickNote16th, bpm, KickNoteOffset16th)
+snareTimestamps = ToTimestamp(SnareNote16th, bpm, SnareNoteOffset16th)
+hiHatTimestamps = ToTimestamp(HiHatNote16th, bpm, HiHatNoteOffset16th)
    
+   #function that generates events from timestamplist, event name and instrument
+def GenerateEvents(timestamps, eventName, instrument):
+    events = []
+    for timestamp in timestamps:
+        event = {
+            'timestamp': timestamp, 
+            'name': eventName,
+            'instrument': instrument
+        }
+        events.append(event)
+    return events
 
 
-#playback
-textureHit = pygame.mixer.Sound("C:/Users/Morris/Desktop/AttractorSamples/CA.textureHit.wav")
-textureHitRev = pygame.mixer.Sound("C:/Users/Morris/Desktop/AttractorSamples/CA.textureHitRev.wav")
-perc1 = pygame.mixer.Sound("C:/Users/Morris/Desktop/AttractorSamples/CA.perc1.wav")
+def getTimestamp(event):
+    return event['timestamp']   
 
-""""
-for repeat in range(numRepeats):
-    print(f"Playback repeat {repeat + 1}/{numRepeats}")
-    timeZero = time.time()  # Reference start time
-    timeStamps = timeStampSeq.copy()  # Reset timestamp list for each repeat
+kickEvents = GenerateEvents(kickTimestamps, 'kickEvent', kick)
+snareEvents = GenerateEvents(snareTimestamps, 'snare', snare)
+hiHatEvents = GenerateEvents(hiHatTimestamps, 'hiHat', hiHat)
 
-    # Loop through each timestamp and play the sound
-    while timeStamps:
+events = kickEvents+snareEvents+hiHatEvents
+events.sort(key=getTimestamp)
+
+#eventHandler
+def handleEvent(event):
+    print(event['timestamp'])
+    print(now-timeZero)
+    event['instrument'].play()
+    print(event['name'])
+   
+   
+timeZero = time.time()
+
+
+while events:
         now = time.time() - timeZero
-        ts = timeStamps[0]  # Get the first timestamp
+        ts = events[0]['timestamp']  # Get the first timestamp
 
         # Check if it's time to play the sound
         if now >= ts:
-            perc1.play()
-            timeStamps.pop(0)  # Remove the played timestamp
+            simultaneousEvents = []
+            
+            while events and events[0]['timestamp'] == ts:
+                simultaneousEvents.append(events.pop(0))
+            for event in simultaneousEvents:
+                     handleEvent(event)
 
-        time.sleep(0.001)  # Small sleep to prevent CPU overuse
+        else:
+         time.sleep(0.001)  # Small sleep to prevent CPU overuse
 
     # Wait for the final note to finish playing before moving to the next repeat
-    time.sleep(timeDurations[-1] + 0.1)  # Add a small buffer to ensure sound finishes
-    """
+time.sleep(1) # Add a small buffer to ensure sound finishes
+   
